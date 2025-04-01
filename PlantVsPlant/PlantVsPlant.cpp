18fc1cfbc142a9ba3e5704c9b92b5b7f73fa90f4
vonbrank <vonbrank@outlook.com>
2025-04-01 23:33:36 +0800

    chore: add and load art resources

--- PlantVsPlant/PlantVsPlant.cpp
+++ PlantVsPlant/PlantVsPlant.cpp
@@ -8,8 +8,79 @@
 #include "util.h"
 #include "Scene/SelectorScene.h"
 
-Scene* menu_scene;
-Scene* game_scene;
+#pragma comment(lib, "winmm.lib")
+
+IMAGE img_menu_background;
+
+IMAGE img_VS;
+IMAGE img_1P;
+IMAGE img_2P;
+IMAGE img_1P_desc;
+IMAGE img_2P_desc;
+IMAGE img_gravestone_left;
+IMAGE img_gravestone_right;
+IMAGE img_selector_tip;
+IMAGE img_selector_background;
+IMAGE img_1P_selector_btn_idle_left;
+IMAGE img_1P_selector_btn_idle_right;
+IMAGE img_1P_selector_btn_down_left;
+IMAGE img_1P_selector_btn_down_right;
+IMAGE img_2P_selector_btn_idle_left;
+IMAGE img_2P_selector_btn_idle_right;
+IMAGE img_2P_selector_btn_down_left;
+IMAGE img_2P_selector_btn_down_right;
+IMAGE img_peashooter_selector_background_left;
+IMAGE img_peashooter_selector_background_right;
+IMAGE img_sunflower_selector_background_left;
+IMAGE img_sunflower_selector_background_right;
+
+IMAGE img_sky;
+IMAGE img_hills;
+IMAGE img_platform_large;
+IMAGE img_platform_small;
+
+IMAGE img_1P_cursor;
+IMAGE img_2P_cursor;
+
+Atlas atlas_peashooter_idle_left;
+Atlas atlas_peashooter_idle_right;
+Atlas atlas_peashooter_run_left;
+Atlas atlas_peashooter_run_right;
+Atlas atlas_peashooter_attack_ex_left;
+Atlas atlas_peashooter_attack_ex_right;
+Atlas atlas_peashooter_die_left;
+Atlas atlas_peashooter_die_right;
+
+Atlas atlas_sunflower_idle_left;
+Atlas atlas_sunflower_idle_right;
+Atlas atlas_sunflower_run_left;
+Atlas atlas_sunflower_run_right;
+Atlas atlas_sunflower_attack_ex_left;
+Atlas atlas_sunflower_attack_ex_right;
+Atlas atlas_sunflower_die_left;
+Atlas atlas_sunflower_die_right;
+
+IMAGE img_pea;
+Atlas atlas_pea_break;
+Atlas atlas_sun;
+Atlas atlas_sun_explode;
+Atlas atlas_sun_ex;
+Atlas atlas_sun_ex_explode;
+Atlas atlas_sun_text;
+
+Atlas atlas_run_effect;
+Atlas atlas_jump_effect;
+Atlas atlas_land_effect;
+
+IMAGE img_1P_winner;
+IMAGE img_2P_winner;
+IMAGE img_winner_bar;
+
+IMAGE img_avatar_peashooter;
+IMAGE img_avatar_sunflower;
+
+Scene* menu_scene = nullptr;
+Scene* game_scene = nullptr;
 Scene* selector_scene = nullptr;
 
 SceneManager scene_manager;
@@ -25,10 +96,101 @@ void flip_atlas(Atlas& src, Atlas& dst)
     }
 }
 
+void load_game_resources()
+{
+    AddFontResourceEx(_T("Assets/resources/IPix.ttf"), FR_PRIVATE, NULL);
+
+    loadimage(&img_menu_background, _T("Assets/resources/menu_background.png"));
+
+    loadimage(&img_VS, _T("Assets/resources/VS.png"));
+    loadimage(&img_1P, _T("Assets/resources/1P.png"));
+    loadimage(&img_2P, _T("Assets/resources/2P.png"));
+    loadimage(&img_1P_desc, _T("Assets/resources/1P_desc.png"));
+    loadimage(&img_2P_desc, _T("Assets/resources/2P_desc.png"));
+    loadimage(&img_gravestone_right, _T("Assets/resources/gravestone.png"));
+    flip_image(&img_gravestone_right, &img_gravestone_left);
+    loadimage(&img_selector_tip, _T("Assets/resources/selector_tip.png"));
+    loadimage(&img_selector_background, _T("Assets/resources/selector_background.png"));
+    loadimage(&img_1P_selector_btn_idle_right, _T("Assets/resources/1P_selector_btn_idle.png"));
+    flip_image(&img_1P_selector_btn_idle_right, &img_1P_selector_btn_idle_left);
+    loadimage(&img_1P_selector_btn_down_right, _T("Assets/resources/1P_selector_btn_down.png"));
+    flip_image(&img_1P_selector_btn_down_right, &img_1P_selector_btn_down_left);
+    loadimage(&img_2P_selector_btn_idle_right, _T("Assets/resources/2P_selector_btn_idle.png"));
+    flip_image(&img_2P_selector_btn_idle_right, &img_2P_selector_btn_idle_left);
+    loadimage(&img_2P_selector_btn_down_right, _T("Assets/resources/2P_selector_btn_down.png"));
+    flip_image(&img_2P_selector_btn_down_right, &img_2P_selector_btn_down_left);
+    loadimage(&img_peashooter_selector_background_right, _T("Assets/resources/peashooter_selector_background.png"));
+    flip_image(&img_peashooter_selector_background_right, &img_peashooter_selector_background_left);
+    loadimage(&img_sunflower_selector_background_right, _T("Assets/resources/sunflower_selector_background.png"));
+    flip_image(&img_sunflower_selector_background_right, &img_sunflower_selector_background_left);
+
+    loadimage(&img_sky, _T("Assets/resources/sky.png"));
+    loadimage(&img_hills, _T("Assets/resources/hills.png"));
+    loadimage(&img_platform_large, _T("Assets/resources/platform_large.png"));
+    loadimage(&img_platform_small, _T("Assets/resources/platform_small.png"));
+
+    loadimage(&img_1P_cursor, _T("Assets/resources/1P_cursor.png"));
+    loadimage(&img_2P_cursor, _T("Assets/resources/2P_cursor.png"));
+
+    atlas_peashooter_idle_right.load_from_file(_T("Assets/resources/peashooter_idle_%d.png"), 9);
+    flip_atlas(atlas_peashooter_idle_right, atlas_peashooter_idle_left);
+    atlas_peashooter_run_right.load_from_file(_T("Assets/resources/peashooter_run_%d.png"), 5);
+    flip_atlas(atlas_peashooter_run_right, atlas_peashooter_run_left);
+    atlas_peashooter_attack_ex_right.load_from_file(_T("Assets/resources/peashooter_attack_ex_%d.png"), 3);
+    flip_atlas(atlas_peashooter_attack_ex_right, atlas_peashooter_attack_ex_left);
+    atlas_peashooter_die_right.load_from_file(_T("Assets/resources/peashooter_die_%d.png"), 4);
+    flip_atlas(atlas_peashooter_die_right, atlas_peashooter_die_left);
+
+    atlas_sunflower_idle_right.load_from_file(_T("Assets/resources/sunflower_idle_%d.png"), 8);
+    flip_atlas(atlas_sunflower_idle_right, atlas_sunflower_idle_left);
+    atlas_sunflower_run_right.load_from_file(_T("Assets/resources/sunflower_run_%d.png"), 5);
+    flip_atlas(atlas_sunflower_run_right, atlas_sunflower_run_left);
+    atlas_sunflower_attack_ex_right.load_from_file(_T("Assets/resources/sunflower_attack_ex_%d.png"), 9);
+    flip_atlas(atlas_sunflower_attack_ex_right, atlas_sunflower_attack_ex_left);
+    atlas_sunflower_die_right.load_from_file(_T("Assets/resources/sunflower_die_%d.png"), 2);
+    flip_atlas(atlas_sunflower_die_right, atlas_sunflower_die_left);
+
+    loadimage(&img_pea, _T("Assets/resources/pea.png"));
+    atlas_pea_break.load_from_file(_T("Assets/resources/pea_break_%d.png"), 3);
+    atlas_sun.load_from_file(_T("Assets/resources/sun_%d.png"), 5);
+    atlas_sun_explode.load_from_file(_T("Assets/resources/sun_explode_%d.png"), 5);
+    atlas_sun_ex.load_from_file(_T("Assets/resources/sun_ex_%d.png"), 5);
+    atlas_sun_ex_explode.load_from_file(_T("Assets/resources/sun_ex_explode_%d.png"), 5);
+    atlas_sun_text.load_from_file(_T("Assets/resources/sun_text_%d.png"), 6);
+
+    atlas_run_effect.load_from_file(_T("Assets/resources/run_effect_%d.png"), 4);
+    atlas_jump_effect.load_from_file(_T("Assets/resources/jump_effect_%d.png"), 5);
+    atlas_land_effect.load_from_file(_T("Assets/resources/land_effect_%d.png"), 2);
+
+    loadimage(&img_1P_winner, _T("Assets/resources/1P_winner.png"));
+    loadimage(&img_2P_winner, _T("Assets/resources/2P_winner.png"));
+    loadimage(&img_winner_bar, _T("Assets/resources/winner_bar.png"));
+
+    loadimage(&img_avatar_peashooter, _T("Assets/resources/avatar_peashooter.png"));
+    loadimage(&img_avatar_sunflower, _T("Assets/resources/avatar_sunflower.png"));
+
+    mciSendString(_T("open Assets/resources/bgm_game.mp3 alias bgm_game"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/bgm_menu.mp3 alias bgm_menu"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/pea_break_1.mp3 alias pea_break_1"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/pea_break_2.mp3 alias pea_break_2"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/pea_break_3.mp3 alias pea_break_3"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/pea_shoot_1.mp3 alias pea_shoot_1"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/pea_shoot_2.mp3 alias pea_shoot_2"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/pea_shoot_ex.mp3 alias pea_shoot_ex"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/sun_explode.mp3 alias sun_explode"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/sun_explode_ex.mp3 alias sun_explode_ex"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/sun_text.mp3 alias sun_text"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/ui_confirm.wav alias ui_confirm"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/ui_switch.wav alias ui_switch"), NULL, 0, NULL);
+    mciSendString(_T("open Assets/resources/ui_win.wav alias ui_win"), NULL, 0, NULL);
+}
+
 int main(int argc, char* argv[])
 {
     ExMessage msg;
     const int FPS = 60;
+
+    load_game_resources();
 
     initgraph(1280, 720);
 
